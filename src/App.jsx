6e3a9e680b22f87d992298b9c1776f69091ff0cb
Gonza <gonzagramaglia@gmail.com>
2023-06-03 01:28:58 -0300

    Branding

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaGithub } from 'react-icons/fa';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <nav id='navbar' className='w-full h-[80px] md:h-[110px] flex items-center bg-[#050816] fixed z-20'>
+        <a 
+          href="https://www.gonza.gr/" 
+          className='ml-[20px] md:ml-[65px]'
+          target="_blank"
+        >
+          <img src='https://i.postimg.cc/9z5zvr3M/logo.png' alt="logo" className='h-[20px] md:h-[25px]' /> 
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a 
+          href="https://github.com/gonzagramaglia/product" 
+          className='ml-auto mr-[20px] md:mr-[65px]'
+          target="_blank"
+        >
+          <FaGithub size={23} style={{ color: '#a9a6c3' }} />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </nav>
     </>
   )
 }
