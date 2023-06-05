8085728476dd8ff211f9caaf584aac28fd6d13d2
Gonza <gonzagramaglia@gmail.com>
2023-06-05 10:48:12 -0300

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ function Home() {
                                 type="filled"
                                 title="Customize It"
                                 handleClick={ () => state.intro = false }
-                                customStyles= "w-fit px-4 py-2.5 font-bold text-sm"
+                                customStyles= "w-fit px-4 py-2.5 mb-36 font-bold text-sm"
                             />
                         </motion.div>
                     </motion.div>
