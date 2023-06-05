8567eae999044d17b45f4a531d95568a3007914f
Gonza <gonzagramaglia@gmail.com>
2023-06-05 12:17:28 -0300

    Refactoring

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
-import config from '../config/config';
 import state from '../store';
-import { download } from '../assets';
-import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
 
-import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+import { ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 function Customizer() {
 
     const snap = useSnapshot(state);
 
     const [file, setFile] = useState('');
-
-    const [prompt, setPrompt] = useState('');
-    const [generatingImg, setGeneratingImg] = useState(false);
 
     const [activeEditorTab, setActiveEditorTab] = useState('');
     const [activeFilterTab, setActiveFilterTab] = useState({
@@ -37,30 +31,11 @@ function Customizer() {
                 setFile={setFile}
                 readFile={readFile}
             />
-          case "aipicker":
-            return <AIPicker 
-                prompt={prompt}
-                setPrompt={setPrompt}
-                generatingImg={generatingImg}
-                handleSubmit={handleSubmit}
-            />
           default:
             return null;
         }
     }
 
-    const handleSubmit = async (type) => {
-        if(!prompt) return alert("Please enter a prompt");
-
-        try{
-
-        } catch(error){
-            alert(error)
-        } finally {
-            setGeneratingImg(false);
-            setActiveEditorTab('')
-        }
-    }
 
     const handleDecals = (type, result) => {
         const decalType = DecalTypes[type];
